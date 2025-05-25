69930d09bb3b6db750fba09fd8dfd5597a25df4b
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-05-25 23:50:51 +0200

    added dotenv

--- WebTech-Backend/src/db.ts
+++ WebTech-Backend/src/db.ts
@@ -1,11 +1,22 @@
 import { Sequelize } from "sequelize";
 
+// const DB_DRIVER = process.env.DB_DRIVER ?? "sqlite";
+// const DB_USER = process.env.DB_USER ?? "";
+// const DB_PASS = process.env.DB_PASS ?? "";
+// const DB_HOST = process.env.DB_HOST ?? "mydb.sqlite";
+// const DB_PORT = process.env.DB_PORT ?? "";
+// const DB_NAME = process.env.DB_NAME ?? "";
+
+// const DB_URL = `${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const DB_URL = `sqlite:mydb.sqlite`;
+
 class DatabaseConnectionManager {
     private static instance: Sequelize;
 
     static getInstance(): Sequelize {
         if (!DatabaseConnectionManager.instance) {
-            DatabaseConnectionManager.instance = new Sequelize(`sqlite:mydb.sqlite`);
+            DatabaseConnectionManager.instance = new Sequelize(DB_URL);
             DatabaseConnectionManager.testConnection();
         }
         return DatabaseConnectionManager.instance;
