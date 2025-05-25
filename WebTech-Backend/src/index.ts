3cc4afe26492afbd0fe80d4e811d3283c7ccaf71
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-05-26 01:15:07 +0200

    added cookie-parser and session examples

--- WebTech-Backend/src/index.ts
+++ WebTech-Backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import { router } from './routers/router.js';
 import { database } from './db.js';
 import './models/associations.js';
@@ -11,6 +13,10 @@ database.sync().then( () => {console.log("Database synchronized.")});
 app.set("view engine", "pug");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+app.use(session({
+secret: "s3cr37", saveUninitialized: false, resave: false
+}));
 app.use(`/`, router);
 
 app.listen(PORT);
