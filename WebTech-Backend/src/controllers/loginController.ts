335dc7e3b8a8eba880e5b434e86290f9c0999f6b
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-05-26 18:57:59 +0200

    implemented login

--- WebTech-Backend/src/controllers/loginController.ts
+++ WebTech-Backend/src/controllers/loginController.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { User } from "../models/User.js";
 
 export function getLogin(req: express.Request, res: express.Response) {
     res.send("login");
 }
 
-export function postLogin(req: express.Request, res: express.Response) {
-    res.send("login post"); 
+export async function postLogin(req: express.Request, res: express.Response) {
+    async function validate(){
+        let found = await User.findOne({
+            where: { username: req.body.usr, password: req.body.pwd }
+        });
+        if(found !== null) {
+            req.session.isAuthenticated = true; 
+            req.session.username = found.dataValues.username;
+            return true;
+        }
+        return false;
+    }
+
+    if (await validate()) {
+        res.send("OK!");
+    } else {
+        res.status(400).send("LOGIN FAILED");
+    }
 }
