4a5b7a5b2397b7075a17d0d6bce2bc885653b55c
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-05-27 11:56:36 +0200

    added homepage and login to app

--- WebTech-Frontend/src/app/app.routes.ts
+++ WebTech-Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        title: "Homepage",
+        component: HomepageComponent,
+    },
+    {
+        path: "login",
+        title: "Login",
+        component: LoginComponent,
+    }
+];
