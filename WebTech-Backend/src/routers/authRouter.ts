3e021bee0eb6b557e82a32dbe2e98d847f3afee3
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-17 20:48:45 +0200

    added signup and swagger comments

--- WebTech-Backend/src/routers/authRouter.ts
+++ WebTech-Backend/src/routers/authRouter.ts
@@ -1,7 +1,64 @@
 import express from 'express';
-import { postLogin } from '../controllers/authController.js';
+import { postLogin, postSignup } from '../controllers/authController.js';
 
 export const authRouter = express.Router();
 
+/**
+ * @swagger
+ *  /auth/login:
+ *    post:
+ *      description: Authenticate user
+ *      produces:
+ *        - application/json
+ *      requestBody:
+ *        description: user credentials to authenticate
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                usr:
+ *                  type: string
+ *                  example: Kyle
+ *                pwd:
+ *                  type: string
+ *                  example: p4ssw0rd
+ *      responses:
+ *        200:
+ *          description: User authenticated
+ *        401:
+ *          description: Invalid credentials
+ */
 authRouter.route(`/login`)
     .post(postLogin);
+
+/**
+ * @swagger
+ *  /auth/signup:
+ *    post:
+ *      description: Register user
+ *      produces:
+ *        - application/json
+ *      requestBody:
+ *        description: user credentials to register
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                usr:
+ *                  type: string
+ *                  example: Kyle
+ *                pwd:
+ *                  type: string
+ *                  example: p4ssw0rd
+ *      responses:
+ *        200:
+ *          description: User authenticated
+ *        401:
+ *          description: Invalid credentials
+ */
+authRouter.route(`/signup`)
+    .post(postSignup);
