d4b7fffbab6ff721731e45f7b00659af0b545fb6
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-17 16:08:43 +0200

    removed pug and express-session, added swagger

--- WebTech-Backend/src/index.ts
+++ WebTech-Backend/src/index.ts
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import session from 'express-session';
+import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import cookieParser from 'cookie-parser';
+// import session from 'express-session';
 import DatabaseConnectionManager from './DatabaseConnectionManager.js';
 import { initializeDatabaseModels } from './initializeDatabaseModels.js';
 import { router } from './routers/router.js';
@@ -10,8 +13,19 @@ import { errorHandler } from './controllers/errorHandler.js';
 dotenv.config();
 
 async function startServer(){
-    const SESSION_SECRET = process.env.SESSION_SECRET ?? (() => { throw new Error("SESSION_SECRET is not set"); })();
+    // const SESSION_SECRET = process.env.SESSION_SECRET ?? (() => { throw new Error("SESSION_SECRET is not set"); })();
+    
     const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+    const swaggerSpec = swaggerJSDoc({
+        definition: {
+            openapi: '3.1.0',
+            info: {
+                title: 'WebTech Backend API',
+                version: '1.0.0',
+            }
+        },
+    });
 
     const database = await DatabaseConnectionManager.getInstance();
     await DatabaseConnectionManager.testConnection();
@@ -19,13 +33,21 @@ async function startServer(){
     await database.sync();
 
     const app = express();
-    app.set("view engine", "pug");
+
+    // app.set("view engine", "pug");
+
+    //middleware
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-    app.use(session({ secret: SESSION_SECRET, saveUninitialized: false, resave: false, }));
-    app.use(`/`, router);
+    // app.use(session({ secret: SESSION_SECRET, saveUninitialized: false, resave: false, }));
     app.use(errorHandler);
+
+    //routers
+    app.use(`/`, router);
+
     app.listen(PORT);
 }
 
