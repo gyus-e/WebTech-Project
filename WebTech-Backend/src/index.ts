eae4552392ad1f7a0017f0a3dae53c4d2660e963
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-17 20:47:15 +0200

    reordered

--- WebTech-Backend/src/index.ts
+++ WebTech-Backend/src/index.ts
@@ -4,9 +4,9 @@ import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import cookieParser from 'cookie-parser';
 import { swaggerSpec } from './swaggerSpec.js';
+import { initializeDatabase } from './database.js';
 import { router } from './routers/router.js';
 import { errorHandler } from './controllers/errorHandler.js';
-import { initializeDatabase } from './database.js';
 
 async function main(){
     dotenv.config();
@@ -17,14 +17,15 @@ async function main(){
 
     const app = express();
 
-    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-    app.use(errorHandler);
-
+    
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.use(`/`, router);
+
+    app.use(errorHandler);
 
     app.listen(PORT);
 }
