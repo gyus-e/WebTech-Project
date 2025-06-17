950f2a9586a8125497eb4d78890e7f517830bffa
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-17 20:47:41 +0200

    improved readability and added signup

--- WebTech-Backend/src/controllers/authController.ts
+++ WebTech-Backend/src/controllers/authController.ts
@@ -3,32 +3,51 @@ import { User } from "../models/User.js"
 import Jwt from "jsonwebtoken";
 
 export async function postLogin(req: express.Request, res: express.Response) {
-    async function checkCredentials(req: express.Request, res: express.Response){
-        let found = await User.findOne({
-            where: { username: req.body.usr, password: req.body.pwd } //assuming password is already hashed
-        });
-        if(found === null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function issueToken(username: string){
-        return Jwt.sign({user:username}, process.env.TOKEN_SECRET ?? (() => { throw new Error("TOKEN_SECRET is not defined"); })(), {
-            expiresIn: `${24*60*60}s`
-        });
-    }
-
-    // function isTokenValid(token, callback) {
-    //     Jwt.verify(token, process.env.TOKEN_SECRET, callback);
-    // }
-
     let isAuthenticated = await checkCredentials(req, res);
     if(isAuthenticated){
         res.json(issueToken(req.body.usr));
     } else {
-        res.status(401);
-        res.json( {error: "Invalid credentials. Try again."});
+        res.status(401).json( {error: "Invalid credentials. Try again."} );
     }
 }
+
+export async function postSignup(req: express.Request, res: express.Response) {
+    try {
+        const user = await User.create({
+            userName: req.body.usr, 
+            password: req.body.pwd
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create user.' });
+        console.error(error);
+    }
+}
+
+async function checkCredentials(req: express.Request, res: express.Response){
+        let found = await User.findOne({
+            where: { 
+                username: req.body.usr, 
+                password: req.body.pwd 
+            }
+        });
+    return found !== null;
+}
+
+function issueToken(username: string){
+    return Jwt.sign(
+        {user:username}, 
+        process.env.TOKEN_SECRET ?? (() => { throw new Error("TOKEN_SECRET is not defined"); })(), 
+        {expiresIn: `${24*60*60}s`}
+    );
+}
+
+//TODO: fix the callback type
+function isTokenValid(token: string, callback: any) {
+    Jwt.verify(
+        token, 
+        process.env.TOKEN_SECRET ?? (() => { throw new Error("TOKEN_SECRET is not defined"); })(),
+        callback
+    );
+}
+
