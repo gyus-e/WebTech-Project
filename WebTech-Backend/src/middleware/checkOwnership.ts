32cba6fc266673e164765512479a1a2ff066172b
gyus-e <giuseppe.amato8@studenti.unina.it>
2025-06-18 20:51:06 +0200

    less redundant

--- WebTech-Backend/src/middleware/checkOwnership.ts
+++ WebTech-Backend/src/middleware/checkOwnership.ts
@@ -3,27 +3,30 @@ import { Photo } from "../models/Photo.js";
 import { Cat } from "../models/Cat.js";
 
 
+type UploadedItem = Photo | Cat;
+
+
+function checkOwnership(item: UploadedItem | null) {
+    return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const user = req.username;
+        if (!item) {
+            return res.status(404).json({ error: "Item not found" });
+        }
+        if (item.uploader !== user) {
+            return res.status(403).json({ error: "Forbidden" });
+        }
+        next();
+    }
+}
+
+
 export async function checkPhotoOwnership(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const user = req.username;
-    const photo = await Photo.findByPk(req.params.photo_id);
-    if (!photo) {
-        return res.status(404).json({ error: "Photo not found" });
-    }
-    if (photo.uploader !== user) {
-        return res.status(403).json({ error: "Forbidden" });
-    }
-    next();
+    const photo = await Photo.findByPk(req.params.photo_id)
+    checkOwnership(photo)(req, res, next);
 }
 
 
 export async function checkCatOwnership(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const user = req.username;
-    const cat = await Cat.findByPk(req.params.cat_id);
-    if (!cat) {
-        return res.status(404).json({ error: "Cat not found" });
-    }
-    if (cat.uploader !== user) {
-        return res.status(403).json({ error: "Forbidden" });
-    }
-    next();
+    const cat = await Cat.findByPk(req.params.cat_id)
+    checkOwnership(cat)(req, res, next);
 }
