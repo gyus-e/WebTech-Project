6d738556e0457f2e08c1f62ee43e9d74594a22d4
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-18 23:10:00 +0200

    renamed WebTech Backend to WebTech Streetcats

--- WebTech-Backend/src/swaggerSpec.ts
+++ WebTech-Backend/src/swaggerSpec.ts
@@ -4,7 +4,7 @@ export const swaggerSpec = swaggerJSDoc({
     definition: {
         openapi: '3.1.0',
         info: {
-            title: 'WebTech Backend API',
+            title: 'WebTech Streetcats API',
             version: '1.0.0',
         }
     },
