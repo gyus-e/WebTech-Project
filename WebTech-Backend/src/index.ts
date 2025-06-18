e81760d30efc3041aeb260150c4056bbaa562654
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-19 00:12:48 +0200

    removed tiny-csrf

--- WebTech-Backend/src/index.ts
+++ WebTech-Backend/src/index.ts
@@ -1,26 +1,28 @@
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
-import cookieParser from 'cookie-parser';
 import { swaggerSpec } from './swaggerSpec.js';
 import { initializeDatabase } from './database.js';
 import { router } from './routes/router.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+
 async function main(){
     dotenv.config();
-    const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+    const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+    const COOKIE_PARSER_SECRET: string = process.env.COOKIE_PARSER_SECRET ?? (() => {throw new Error("Cookie parser secret is not set in environment variables");})();
 
     const database = await initializeDatabase();
     await database.sync();
 
     const app = express();
 
-    app.use(cors());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser(COOKIE_PARSER_SECRET));
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser());
+    app.use(cors());
     
     app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.use(`/`, router);
