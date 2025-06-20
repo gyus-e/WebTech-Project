505d99ec3228a39088afc50ddde1adb00103f644
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-06-20 20:52:11 +0200

    better homepage and login

--- WebTech-Frontend/src/app/homepage/homepage.component.ts
+++ WebTech-Frontend/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../_services/auth/auth.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-
+  authService = inject(AuthService);
 }
